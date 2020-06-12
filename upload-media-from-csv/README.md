924099c009d3a75d072ccc483dac185fa785ed54
bln-kth <66303636+bln-kth@users.noreply.github.com>
2020-06-12 12:38:29 +0200

    Add files via upload

---
+++ upload-media-from-csv/README.md
@@ -0,0 +1,20 @@
+# kaltura-upload-media
+Upload media entry from csv file. Only media type Video
+
+### api url
+* Public site = `www.kaltura.com`
+* Nordunet site = `api.kaltura.nordu.net`
+
+## To run
+* Install/Download python:
+https://www.python.org/downloads/
+* Install/Download pip
+https://pip.pypa.io/en/stable/installing/
+* Install deps:
+`pip install KalturaApiClient`
+* Run the script:
+`python upload-media.py example.csv`
+
+### Info
+>This is Open Source and it´s free to use or change.\
+Use this script at **your own risk**. There is **no** support or help included.
---
+++ upload-media-from-csv/upload-media.py
@@ -0,0 +1,62 @@
+import sys, csv
+from pprint import pprint
+from KalturaClient import *
+from KalturaClient.Plugins.Core import *
+import private
+
+try:
+	csv_file = sys.argv[1]
+except:
+	csv_file = input("csv file: ")
+else:
+	csv_file = sys.argv[1]
+
+config = KalturaConfiguration()
+config.serviceUrl = "https://api.kaltura.nordu.net/"
+client = KalturaClient(config)
+ks = client.session.start(
+	private.secret,
+	private.adminuser_id,
+	KalturaSessionType.ADMIN,
+	private.partner_id)
+client.setKs(ks)
+
+f = open(csv_file)
+csv_f = csv.reader(f)
+
+for row in csv_f:
+	if not str(row[0]).startswith('#'):
+		name = row[0]
+		userid = row[1]
+		tags = row[2]
+		description = row[3]
+		filename = row[4]
+		
+		uploadToken = KalturaUploadToken()
+
+		result = client.uploadToken.add(uploadToken)
+		#pprint(vars(result))
+		uploadTokenId = result.id
+		fileData = open(filename, 'rb')
+		resume = False
+		finalChunk = True
+		resumeAt = -1
+
+		result = client.uploadToken.upload(uploadTokenId, fileData, resume, finalChunk, resumeAt)
+		#pprint(vars(result))
+		entry = KalturaMediaEntry()
+		entry.mediaType = KalturaMediaType.VIDEO
+		entry.name = name
+		entry.userId = userid
+		entry.tags = tags
+		entry.description = description
+
+		result = client.media.add(entry)
+		#pprint(vars(result))
+		entryId = result.id
+		#print(entryId)
+		resource = KalturaUploadedFileTokenResource()
+		resource.token = uploadTokenId
+
+		result = client.media.addContent(entryId, resource)
+		#pprint(vars(result))
