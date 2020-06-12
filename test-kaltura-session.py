0b86d7cae3f2cad2c57f6753c3485c9e8722ab53
Björn Larsson <bjornlarsson@bln-macbook.local>
2020-06-12 14:07:42 +0200

    update

--- test-kaltura-session.py
+++ test-kaltura-session.py
@@ -1,1 +1,15 @@
+from KalturaClient import *
+from KalturaClient.Plugins.Core import *
 
+sys.path.append('../')
+import private
+
+config = KalturaConfiguration()
+config.serviceUrl = "https://api.kaltura.nordu.net/"
+client = KalturaClient(config)
+ks = client.session.start(
+	private.secret,
+	private.adminuser_id,
+	KalturaSessionType.ADMIN,
+	private.partner_id)
+print (client.setKs(ks))
